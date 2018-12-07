ba34f2f9c87349ba8fccd4b28516c9d35464c167
Ana Milovanovic <a.milovanovic@levi9.com>
2018-12-07 14:24:22 +0100

    Share button added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { SocialSharing } from '@ionic-native/social-sharing';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,16 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private socialSharing: SocialSharing) {
 
+  }
+  compilemsg():string{
+    var msg = 'Sent from ARonia!' ;
+    return msg;
+  }
+  regularShare(){
+    var msg = this.compilemsg();
+    this.socialSharing.share(msg, null, null, null);
   }
 
 }
